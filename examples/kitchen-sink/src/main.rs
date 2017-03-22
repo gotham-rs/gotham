e3a99bef65eae0ab338a381d349280b883c570f5
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2017-03-23 06:03:34 +1000

    Add sample hyper app as baseline

---
+++ examples/kitchen-sink/src/main.rs
@@ -0,0 +1,51 @@
+#![deny(warnings)]
+extern crate futures;
+extern crate hyper;
+extern crate pretty_env_logger;
+
+use futures::future::FutureResult;
+
+use hyper::{Get, Post, StatusCode};
+use hyper::header::ContentLength;
+use hyper::server::{Http, Service, Request, Response};
+
+static INDEX: &'static [u8] = b"Try POST /echo";
+
+#[derive(Clone, Copy)]
+struct Echo;
+
+impl Service for Echo {
+    type Request = Request;
+    type Response = Response;
+    type Error = hyper::Error;
+    type Future = FutureResult<Response, hyper::Error>;
+
+    fn call(&self, req: Request) -> Self::Future {
+        futures::future::ok(match (req.method(), req.path()) {
+            (&Get, "/") | (&Get, "/echo") => {
+                Response::new()
+                    .with_header(ContentLength(INDEX.len() as u64))
+                    .with_body(INDEX)
+            }
+            (&Post, "/echo") => {
+                let mut res = Response::new();
+                if let Some(len) = req.headers().get::<ContentLength>() {
+                    res.headers_mut().set(len.clone());
+                }
+                res.with_body(req.body())
+            }
+            _ => Response::new().with_status(StatusCode::NotFound),
+        })
+    }
+}
+
+
+fn main() {
+    pretty_env_logger::init().unwrap();
+    let addr = "127.0.0.1:1337".parse().unwrap();
+
+    let server = Http::new().bind(&addr, || Ok(Echo)).unwrap();
+    println!("Listening on http://{} with 1 thread.",
+             server.local_addr().unwrap());
+    server.run().unwrap();
+}
