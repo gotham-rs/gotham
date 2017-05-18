282f4d0a8d2baff3b282559a1e7a28786ef5526d
Bradley Beddoes <bradleybeddoes@gmail.com>
2017-05-18 20:17:30 +1000

    Prevent future errors in docs from going unnoticed
    This is the same approach as used in stdlib https://github.com/rust-lang/rust/blob/e326e86b47edcded3313944a81f126a633032d86/src/libstd/lib.rs#L221
    which I discovered via https://github.com/rust-lang/rust/issues/41574.

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,7 @@
 //! [mio]: https://github.com/carllerche/mio
 
 #![warn(missing_docs)]
+#![doc(test(no_crate_inject, attr(deny(warnings))))]
 // TODO: Remove this when it's a hard error by default (error E0446).
 // See Rust issue #34537 <https://github.com/rust-lang/rust/issues/34537>
 #![deny(private_in_public)]
