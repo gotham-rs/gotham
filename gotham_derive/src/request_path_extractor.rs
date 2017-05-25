e7cbc6ae957393d9fb005ec4c553a655bcb8f6e5
Bradley Beddoes <bradleybeddoes@gmail.com>
2017-05-25 20:30:57 +1000

    Correct types that got messed up during rebase

--- gotham_derive/src/request_path_extractor.rs
+++ gotham_derive/src/request_path_extractor.rs
@@ -77,10 +77,10 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
         {
             fn extract(s: &mut gotham::state::State, mut sm: gotham::router::tree::SegmentMapping)
                 -> Result<(), String> {
-                fn parse<T>(segments: Option<&Vec<String>>) -> Result<T, String> where T: gotham::http::request_path::FromRequestPath {
+                fn parse<T>(segments: Option<&Vec<&str>>) -> Result<T, String> where T: gotham::http::request_path::FromRequestPath {
                     match segments {
                         Some(segments) => {
-                            match T::from_request_path(segments) {
+                            match T::from_request_path(segments.as_slice()) {
                                 Ok(val) => Ok(val),
                                 Err(_) => Err(format!("Error converting segments {:?}", segments)),
                             }
