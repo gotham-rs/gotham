7d9f7ed63fde98c947f56682794ea46774d0df19
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2017-07-25 05:28:45 +1000

    Fix references to renamed/moved types after rebase

--- examples/todo-session/src/apps/todo/boot/router.rs
+++ examples/todo-session/src/apps/todo/boot/router.rs
@@ -3,16 +3,18 @@ use std::sync::Arc;
 use hyper::{Method, StatusCode};
 use futures::{future, Future};
 
-use gotham::dispatch::{new_pipeline_set, PipelineSet, PipelineHandleChain, DispatcherImpl};
-use gotham::http::request_path::NoopRequestPathExtractor;
-use gotham::http::query_string::NoopQueryStringExtractor;
 use gotham::handler::NewHandler;
 use gotham::middleware::pipeline::new_pipeline;
 use gotham::middleware::session::NewSessionMiddleware;
 use gotham::router::Router;
 use gotham::router::route::{Extractors, Route, RouteImpl, Delegation};
-use gotham::router::request_matcher::MethodOnlyRequestMatcher;
-use gotham::router::response_extender::{ResponseExtenderBuilder, NoopExtender};
+use gotham::router::route::dispatch::{new_pipeline_set, PipelineSet, PipelineHandleChain,
+                                      DispatcherImpl};
+use gotham::router::route::matcher::MethodOnlyRouteMatcher;
+use gotham::router::request::path::NoopPathExtractor;
+use gotham::router::request::query_string::NoopQueryStringExtractor;
+use gotham::router::response::finalizer::ResponseFinalizerBuilder;
+use gotham::router::response::extender::NoopResponseExtender;
 use gotham::router::tree::TreeBuilder;
 
 use apps::todo::Session;
@@ -27,10 +29,9 @@ fn static_route<NH, P, C>(methods: Vec<Method>,
           C: PipelineHandleChain<P> + Send + Sync + 'static,
           P: Send + Sync + 'static
 {
-    let matcher = MethodOnlyRequestMatcher::new(methods);
+    let matcher = MethodOnlyRouteMatcher::new(methods);
     let dispatcher = DispatcherImpl::new(new_handler, active_pipelines, pipeline_set);
-    let extractors: Extractors<NoopRequestPathExtractor, NoopQueryStringExtractor> =
-        Extractors::new();
+    let extractors: Extractors<NoopPathExtractor, NoopQueryStringExtractor> = Extractors::new();
     let route = RouteImpl::new(matcher,
                                Box::new(dispatcher),
                                extractors,
@@ -62,12 +63,12 @@ pub fn router() -> Router {
 
     let tree = tree_builder.finalize();
 
-    let mut response_extender_builder = ResponseExtenderBuilder::new();
-    let extender_200 = NoopExtender::new();
-    response_extender_builder.add(StatusCode::Ok, Box::new(extender_200));
-    let extender_500 = NoopExtender::new();
-    response_extender_builder.add(StatusCode::InternalServerError, Box::new(extender_500));
-    let response_extender = response_extender_builder.finalize();
+    let mut response_finalizer_builder = ResponseFinalizerBuilder::new();
+    let extender_200 = NoopResponseExtender::new();
+    response_finalizer_builder.add(StatusCode::Ok, Box::new(extender_200));
+    let extender_500 = NoopResponseExtender::new();
+    response_finalizer_builder.add(StatusCode::InternalServerError, Box::new(extender_500));
+    let response_finalizer = response_finalizer_builder.finalize();
 
-    Router::new(tree, response_extender)
+    Router::new(tree, response_finalizer)
 }
