57fa43daa6a1d42ac7c9e50fa96e52e2bc410790
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2017-07-26 05:48:27 +1000

    Add session cleanup logic

--- src/middleware/session/backend.rs
+++ src/middleware/session/backend.rs
@@ -1,7 +1,8 @@
 use super::*;
 
-use std::sync::{Arc, Mutex, PoisonError};
-use std::time::Instant;
+use std::sync::{Arc, Weak, Mutex, PoisonError};
+use std::time::{Instant, Duration};
+use std::thread;
 
 use linked_hash_map::LinkedHashMap;
 
@@ -32,9 +33,22 @@ pub struct MemoryBackend {
     storage: Arc<Mutex<LinkedHashMap<String, (Instant, Vec<u8>)>>>,
 }
 
+impl MemoryBackend {
+    pub fn new(ttl: Duration) -> MemoryBackend {
+        let storage = Arc::new(Mutex::new(LinkedHashMap::new()));
+
+        {
+            let storage = Arc::downgrade(&storage);
+            thread::spawn(move || cleanup_loop(storage, ttl));
+        }
+
+        MemoryBackend { storage }
+    }
+}
+
 impl Default for MemoryBackend {
     fn default() -> MemoryBackend {
-        MemoryBackend { storage: Arc::new(Mutex::new(LinkedHashMap::new())) }
+        MemoryBackend::new(Duration::from_secs(3600))
     }
 }
 
@@ -77,5 +91,93 @@ impl Backend for MemoryBackend {
                 unreachable!("session memory backend lock poisoned, HashMap panicked?")
             }
         }
+    }
+}
+
+fn cleanup_loop(storage: Weak<Mutex<LinkedHashMap<String, (Instant, Vec<u8>)>>>, ttl: Duration) {
+    loop {
+        // If the original `Arc<_>` goes away, we don't need to keep sweeping the cache, because
+        // it's gone too. We can bail out of this thread when the weak ref fails to upgrade.
+        let storage = match storage.upgrade() {
+            None => break,
+            Some(storage) => storage,
+        };
+
+        let duration = match storage.lock() {
+            Err(PoisonError { .. }) => break,
+            Ok(mut storage) => cleanup_once(&mut storage, ttl),
+        };
+
+        if let Some(duration) = duration {
+            thread::sleep(duration);
+        }
+    }
+}
+
+fn cleanup_once(storage: &mut LinkedHashMap<String, (Instant, Vec<u8>)>,
+                ttl: Duration)
+                -> Option<Duration> {
+    match storage.front() {
+        Some((_, &(instant, _))) => {
+            let age = instant.elapsed();
+
+            if age >= ttl {
+                storage.pop_front();
+                None
+            } else {
+                Some(ttl - age)
+            }
+        }
+        None => Some(ttl),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn cleanup_test() {
+        let mut storage = LinkedHashMap::new();
+
+        storage.insert("abcd".to_owned(),
+                       (Instant::now() - Duration::from_secs(3601), vec![]));
+
+        cleanup_once(&mut storage, Duration::from_secs(3600));
+        assert!(storage.is_empty());
+    }
+
+    #[test]
+    fn cleanup_join_test() {
+        let storage = Arc::new(Mutex::new(LinkedHashMap::new()));
+        let weak = Arc::downgrade(&storage);
+
+        let handle = thread::spawn(move || cleanup_loop(weak, Duration::from_millis(1)));
+
+        drop(storage);
+        handle.join().unwrap();
+    }
+
+    #[test]
+    fn memory_backend_test() {
+        let new_backend = MemoryBackend::new(Duration::from_millis(100));
+        let bytes: Vec<u8> = (0..64).map(|_| rand::random()).collect();
+        let identifier = new_backend.new_backend().unwrap().random_identifier();
+
+        new_backend
+            .new_backend()
+            .expect("can't create backend for write")
+            .persist_session(identifier.clone(), &bytes[..])
+            .expect("failed to persist");
+
+        let received = new_backend
+            .new_backend()
+            .expect("can't create backend for read")
+            .read_session(identifier)
+            .wait()
+            .expect("no response from backend")
+            .expect("session data missing");
+
+        assert_eq!(bytes, received);
     }
 }
