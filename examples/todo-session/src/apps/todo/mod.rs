60274f38e0fbd68d066e3f605662a2832b41d704
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2017-07-30 06:38:43 +1000

    Remove todo-session project

--- examples/todo-session/src/apps/todo/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod boot;
-mod controllers;
-
-#[derive(Default, Serialize, Deserialize)]
-struct Session {
-    todo_list: Vec<String>,
-}
--- examples/todo-session/src/bin.rs
+++
@@ -1,5 +0,0 @@
-extern crate todo_session_lib;
-
-fn main() {
-    todo_session_lib::start()
-}
