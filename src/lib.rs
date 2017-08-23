1aa51f14fd41ab853b70110cb7c01d5059b20f53
Bradley Beddoes <bradleybeddoes@gmail.com>
2017-08-23 12:20:46 +1000

    Add Diesel
    Note this explictly overrides diesel to current master which allows
    use to work around an issue that is present in 0.15.2 with R2D2 pools and
    Mysql connections (which conveniently is the first RDBMS this needs to
    work with for demo purposes).

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,9 @@ extern crate log;
 extern crate futures;
 extern crate hyper;
 extern crate gotham;
+#[macro_use]
+extern crate gotham_derive;
 extern crate diesel;
-
-// Enable to use #[derive(StateData)] below
-//#[macro_use]
-//extern crate gotham_derive;
 
 use std::io;
 
@@ -20,15 +18,10 @@ use gotham::middleware::{NewMiddleware, Middleware};
 use gotham::state::{State, request_id};
 use gotham::handler::HandlerFuture;
 
-// Example of struct that stores owned data in State
-//
-// n.b. There is no requirement to have a StateData struct associated with your Middleware
-// instance but it is a common need hence we've shown one here to assist newcomers.
-//
-//#[derive(StateData)]
-//pub struct DieselData {
-//  pub my_value: String,
-//}
+/// Provides access to a Diesel controller within an R2D2 pool via Gotham State
+/// for Middleware and Handlers.
+#[derive(StateData)]
+pub struct DieselData {}
 
 pub struct DieselMiddleware {}
 
@@ -46,19 +39,11 @@ impl Middleware for DieselMiddleware {
         Chain: FnOnce(State, Request) -> Box<HandlerFuture>,
     {
         trace!("[{}] pre chain", request_id(&state));
-        // Do things prior to passing the request on to other middleware and the eventual Handler
-        // ..
-        // For example store something in State
-        // state.put(DieselData { my_value: "abcdefg".to_owned() });
 
         chain(state, request)
             .and_then(move |(state, response)| {
                 {
                     trace!("[{}] post chain", request_id(&state));
-                    // Do things once a response has come back
-                    // ..
-                    // For example get our data back from State
-                    // let data = state.borrow::<DieselData>().unwrap();
                 }
                 future::ok((state, response))
             })
