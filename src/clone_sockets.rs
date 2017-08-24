744dbd050f5d11bbe67e549d13c3db2139098f40
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2017-08-25 06:17:48 +1000

    Support Windows via socket queue implementation
    Attempting to use cloned sockets on Windows resulted in a lot of errors
    from Tokio's TcpListener, and no successful responses. Exploration of
    how to concurrently serve a single TcpListener on Windows was
    unsuccessful, leading to the solution here where a single thread accepts
    connections and then dispatches them via a queue to the per-thread Tokio
    reactors.

---
+++ src/clone_sockets.rs
@@ -0,0 +1,60 @@
+use std::net::{SocketAddr, ToSocketAddrs, TcpListener};
+use std::thread;
+use std::sync::Arc;
+
+use hyper::server::{Http, NewService};
+use tokio_core;
+use tokio_core::reactor::Core;
+use futures::Stream;
+
+use handler::{NewHandler, NewHandlerService};
+
+pub fn start_with_num_threads<NH, A>(addr: A, threads: usize, new_handler: NH)
+where
+    NH: NewHandler + 'static,
+    A: ToSocketAddrs,
+{
+    let (listener, addr) = super::tcp_listener(addr);
+
+    let protocol = Arc::new(Http::new());
+    let service = NewHandlerService::new(new_handler);
+
+    info!(
+        target: "gotham::start",
+        " Gotham listening on http://{} with {} threads",
+        addr,
+        threads,
+    );
+
+    for _ in 0..threads - 1 {
+        let listener = listener.try_clone().expect("unable to clone TCP listener");
+        let protocol = protocol.clone();
+        let service = service.clone();
+        thread::spawn(move || serve(listener, &addr, &protocol, &service));
+    }
+
+    serve(listener, &addr, &protocol, &service);
+}
+
+fn serve<NH>(
+    listener: TcpListener,
+    addr: &SocketAddr,
+    protocol: &Http,
+    new_service: &NewHandlerService<NH>,
+) where
+    NH: NewHandler + 'static,
+{
+    let mut core = Core::new().expect("unable to spawn tokio reactor");
+    let handle = core.handle();
+
+    let listener = tokio_core::net::TcpListener::from_listener(listener, addr, &handle)
+        .expect("unable to convert TCP listener to tokio listener");
+
+    core.run(listener.incoming().for_each(|(socket, addr)| {
+        match new_service.new_service() {
+            Ok(service) => protocol.bind_connection(&handle, socket, addr, service),
+            Err(e) => error!(" unable to spawn service: {:?}", e),
+        }
+        Ok(())
+    })).expect("unable to run reactor over listener");
+}
