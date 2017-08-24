ca4ed8d9a78ad613a6f49d160cdfda2393e5e701
Bradley Beddoes <bradleybeddoes@gmail.com>
2017-08-24 20:36:25 +1000

    Initial impl/concept for Diesel middleware
    For peer review and refinement.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,13 @@
-#![deny(warnings)]
+//! Makes a Diesel connection available to every Middleware and Handler that is involved in
+//! processing a single Gotham request.
+//!
+//! Utilises r2d2 pooling to ensure efficent database usage and prevent resource exhaustion.
+
+#![warn(missing_docs, deprecated)]
+#![doc(test(no_crate_inject, attr(deny(warnings))))]
+// TODO: Remove this when it's a hard error by default (error E0446).
+// See Rust issue #34537 <https://github.com/rust-lang/rust/issues/34537>
+#![deny(private_in_public)]
 
 #[macro_use]
 extern crate log;
@@ -9,6 +18,9 @@ extern crate gotham;
 extern crate gotham_derive;
 extern crate diesel;
 extern crate r2d2;
+extern crate r2d2_diesel;
+
+pub mod state_data;
 
 use std::io;
 
@@ -19,27 +31,71 @@ use gotham::middleware::{NewMiddleware, Middleware};
 use gotham::state::{State, request_id};
 use gotham::handler::HandlerFuture;
 
-/// Provides access to a Diesel controller within an R2D2 pool via Gotham State
-/// for Middleware and Handlers.
-#[derive(StateData)]
-pub struct DieselData {}
+use diesel::Connection;
+use r2d2::Pool;
+use r2d2_diesel::ConnectionManager;
 
-pub struct DieselMiddleware {}
+use state_data::Diesel;
 
-impl NewMiddleware for DieselMiddleware {
-    type Instance = DieselMiddleware;
+/// A Gotham compatible Middleware that manages a pool of Diesel connections via r2d2 and hands
+/// out connections to other Middleware and Handlers that require them via the Gotham `State`
+/// mechanism.
+pub struct DieselMiddleware<T>
+where
+    T: Connection + Send + 'static,
+{
+    pool: r2d2::Pool<ConnectionManager<T>>,
+}
 
-    fn new_middleware(&self) -> io::Result<Self::Instance> {
-        Ok(DieselMiddleware { ..*self })
+impl<T> DieselMiddleware<T>
+where
+    T: Connection + Send + 'static,
+{
+    /// Sets up a new instance of the middleware and establishes a connection to the database.
+    ///
+    /// * The database to connect to, including authentication components.
+    /// * An optional config instance. Utilises `r2d2::Config::default()` if not provided.
+    ///
+    /// # Panics
+    /// If the database identified in `database_url` cannot be connected to at application start.
+    ///
+    /// n.b. connection will be re-established if the database goes away and returns mid execution
+    /// without panic.
+    pub fn new(database_url: &str, c: Option<r2d2::Config<T, r2d2_diesel::Error>>) -> Self {
+        let manager = ConnectionManager::<T>::new(database_url);
+        let r2d2_config = match c {
+            Some(c) => c,
+            None => r2d2::Config::default(),
+        };
+        let pool = Pool::<ConnectionManager<T>>::new(r2d2_config, manager)
+            .expect("Failed to create pool.");
+
+        DieselMiddleware { pool }
     }
 }
 
-impl Middleware for DieselMiddleware {
-    fn call<Chain>(self, state: State, request: Request, chain: Chain) -> Box<HandlerFuture>
+impl<T> NewMiddleware for DieselMiddleware<T>
+where
+    T: Connection + Send + 'static,
+{
+    type Instance = DieselMiddleware<T>;
+
+    fn new_middleware(&self) -> io::Result<Self::Instance> {
+        let pool = self.pool.clone();
+        Ok(DieselMiddleware { pool })
+    }
+}
+
+impl<T> Middleware for DieselMiddleware<T>
+where
+    T: Connection + Send + 'static,
+{
+    fn call<Chain>(self, mut state: State, request: Request, chain: Chain) -> Box<HandlerFuture>
     where
         Chain: FnOnce(State, Request) -> Box<HandlerFuture>,
     {
         trace!("[{}] pre chain", request_id(&state));
+        state.put(Diesel::<T>::new(self.pool));
 
         chain(state, request)
             .and_then(move |(state, response)| {
---
+++ src/state_data.rs
@@ -0,0 +1,45 @@
+//! Defines data structure for storage in Gotham State that provides access to the underlying r2d2
+//! pool so a connection can be established if required by Middleware or Handlers.
+
+use diesel::Connection;
+use r2d2::{GetTimeout, Pool, PooledConnection};
+use r2d2_diesel::ConnectionManager;
+
+use gotham;
+use gotham::state::{State, FromState};
+
+/// Convenience function for usage within 3rd party Middleware and Handlers to obtain a
+/// Diesel connection.
+///
+// Really just addresses the need to include all the relevant types in 3rd
+// party apps but it made working with this middleware a lot nicer.
+pub fn conn<T>(s: &State) -> Result<PooledConnection<ConnectionManager<T>>, GetTimeout>
+where
+    T: Connection + Send + 'static,
+{
+    Diesel::borrow_from(s).conn()
+}
+
+/// Provides access to a Diesel connection within an r2d2 pool via Gotham State
+/// for Middleware and Handlers.
+#[derive(StateData, FromState)]
+pub struct Diesel<T>
+where
+    T: Connection + Send + 'static,
+{
+    pool: Pool<ConnectionManager<T>>,
+}
+
+impl<T> Diesel<T>
+where
+    T: Connection + Send + 'static,
+{
+    pub(crate) fn new(pool: Pool<ConnectionManager<T>>) -> Self {
+        Diesel { pool }
+    }
+
+    /// Provides access to a Diesel connection from our r2d2 backed connection pool.
+    pub fn conn(&self) -> Result<PooledConnection<ConnectionManager<T>>, GetTimeout> {
+        self.pool.get()
+    }
+}
