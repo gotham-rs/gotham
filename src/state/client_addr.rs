de5edfb2d45f175fe3a1bb00258d7823637d84ce
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2017-09-15 18:07:15 +1000

    Add doc example for client_addr function

--- src/state/client_addr.rs
+++ src/state/client_addr.rs
@@ -15,6 +15,40 @@ pub(crate) fn put_client_addr(state: &mut State, addr: SocketAddr) {
 
 /// Returns the client `SocketAddr` as reported by hyper, if one was present. Certain connections
 /// do not report a client address, in which case this will return `None`.
+///
+/// # Examples
+///
+/// ```rust
+/// # extern crate gotham;
+/// # extern crate hyper;
+/// #
+/// # use hyper::{Response, StatusCode};
+/// # use gotham::handler::NewHandlerService;
+/// # use gotham::state::{State, client_addr};
+/// # use gotham::test::TestServer;
+/// #
+/// fn my_handler(state: State) -> (State, Response) {
+///     let addr = client_addr(&state).expect("no client address");
+///     let body = format!("{}", addr);
+///     let response = Response::new().with_status(StatusCode::Ok).with_body(body);
+///     (state, response)
+/// }
+/// #
+/// # fn main() {
+/// #   let new_service = NewHandlerService::new(|| Ok(my_handler));
+/// #   let mut test_server = TestServer::new(new_service).unwrap();
+/// #   let uri = "http://localhost/".parse().unwrap();
+/// #   let response = test_server
+/// #       .client("127.0.0.1:9816".parse().unwrap())
+/// #       .unwrap()
+/// #       .get(uri);
+/// #
+/// #   let response = test_server.run_request(response).unwrap();
+/// #
+/// #   assert_eq!(response.status(), StatusCode::Ok);
+/// #   let buf = test_server.read_body(response).unwrap();
+/// #   assert_eq!(buf.as_slice(), b"127.0.0.1:9816");
+/// # }
 pub fn client_addr(state: &State) -> Option<SocketAddr> {
     ClientAddr::try_borrow_from(&state).map(|c| c.addr)
 }
