55d26be06b6e5ff460bad96f1707d0f5a9f5b0cd
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2018-01-03 19:32:56 +1000

    Add `head` route function

--- src/router/builder/draw.rs
+++ src/router/builder/draw.rs
@@ -105,6 +105,43 @@ where
         self.request(vec![Method::Get], path)
     }
 
+    /// Creates a route which matches `HEAD` requests to the given path.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # extern crate gotham;
+    /// # extern crate hyper;
+    /// # use hyper::Response;
+    /// # use gotham::state::State;
+    /// # use gotham::router::Router;
+    /// # use gotham::router::builder::*;
+    /// # use gotham::middleware::pipeline::new_pipeline;
+    /// # use gotham::middleware::session::NewSessionMiddleware;
+    /// # use gotham::router::route::dispatch::{new_pipeline_set, finalize_pipeline_set};
+    /// # fn my_handler(_: State) -> (State, Response) {
+    /// #   unreachable!()
+    /// # }
+    /// #
+    /// # fn router() -> Router {
+    /// #   let pipelines = new_pipeline_set();
+    /// #   let (pipelines, default) =
+    /// #       pipelines.add(new_pipeline().add(NewSessionMiddleware::default()).build());
+    /// #
+    /// #   let pipelines = finalize_pipeline_set(pipelines);
+    /// #
+    /// #   let default_pipeline_chain = (default, ());
+    /// #
+    /// build_router(default_pipeline_chain, pipelines, |route| {
+    ///     route.head("/request/path").to(my_handler);
+    /// })
+    /// # }
+    /// # fn main() { router(); }
+    /// ```
+    fn head<'b>(&'b mut self, path: &str) -> DefaultSingleRouteBuilder<'b, C, P> {
+        self.request(vec![Method::Head], path)
+    }
+
     /// Creates a route which matches `POST` requests to the given path.
     ///
     /// # Examples
