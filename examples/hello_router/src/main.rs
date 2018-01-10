b3e85b4e85f5017d88d5d10ed09ac162033a0e3f
Bradley Beddoes <bradleybeddoes@gmail.com>
2018-01-10 21:10:44 +1000

    Add the hello_router example
    Builds on the Hello World example making use of a Router to
    distribute requests to the same Handler.

---
+++ examples/hello_router/src/main.rs
@@ -0,0 +1,59 @@
+//! A Hello World example application for working with the Gotham Router.
+
+extern crate futures;
+extern crate gotham;
+extern crate hyper;
+extern crate mime;
+
+use hyper::{Response, StatusCode};
+
+use gotham::http::response::create_response;
+use gotham::state::State;
+use gotham::router::Router;
+use gotham::router::builder::{build_simple_router, DefineSingleRoute, DrawRoutes};
+
+/// Create a `Handler`
+pub fn say_hello(state: State) -> (State, Response) {
+    let res = create_response(
+        &state,
+        StatusCode::Ok,
+        Some((String::from("Hello Router!").into_bytes(), mime::TEXT_PLAIN)),
+    );
+
+    (state, res)
+}
+
+/// Create a `Router`
+fn router() -> Router {
+    build_simple_router(|route| {
+        route.get("/").to(say_hello);
+    })
+}
+
+/// Start a server and use a `Router` to dispatch requests
+pub fn main() {
+    let addr = "127.0.0.1:7878";
+    println!("Listening for requests at http://{}", addr);
+    gotham::start(addr, router())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use gotham::test::TestServer;
+
+    #[test]
+    fn recieve_hello_router_response() {
+        let test_server = TestServer::new(router()).unwrap();
+        let response = test_server
+            .client()
+            .get("http://localhost")
+            .perform()
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::Ok);
+
+        let body = response.read_body().unwrap();
+        assert_eq!(&body[..], b"Hello Router!");
+    }
+}
