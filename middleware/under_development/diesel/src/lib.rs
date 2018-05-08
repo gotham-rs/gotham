cf80280e491f1da20c92d95c4d3a5b3215ac2bd9
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2018-05-08 19:46:37 +1000

    Rewrite diesel middleware docs to include examples

--- middleware/under_development/diesel/src/lib.rs
+++ middleware/under_development/diesel/src/lib.rs
@@ -1,7 +1,139 @@
-//! Makes a Diesel connection available to Middleware and Handlers that are involved in
-//! processing a Request.
+//! Provides an interface for running Diesel queries in a Gotham application.
 //!
-//! Utilises r2d2 pooling to ensure efficent database usage and prevent resource exhaustion.
+//! # Installing the middleware
+//!
+//! Correct usage of the `DieselMiddleware` requires the `WorkersMiddleware`. Ensure both
+//! middleware are in the pipeline used by your routes.
+//!
+//! ```rust
+//! # extern crate gotham;
+//! # extern crate gotham_middleware_workers;
+//! # extern crate gotham_middleware_diesel;
+//! # extern crate diesel;
+//! #
+//! # use gotham::router::Router;
+//! # use gotham::router::builder::*;
+//! # use gotham::pipeline::single::*;
+//! # use gotham::pipeline::*;
+//! # use gotham_middleware_workers::*;
+//! # use gotham_middleware_diesel::*;
+//! # use diesel::SqliteConnection;
+//! #
+//! fn router() -> Router {
+//!     let (chain, pipelines) = single_pipeline(
+//!         new_pipeline()
+//!             // The middleware from `gotham_middleware_workers`, to manage
+//!             // the thread pool which `DieselMiddleware` requires.
+//!             .add(WorkersMiddleware::new(20))
+//!             // Initialize `DieselMiddleware` with a connection string / URL.
+//!             .add(DieselMiddleware::<SqliteConnection>::new(":memory:"))
+//!             .build()
+//!     );
+//!
+//!     build_router(chain, pipelines, |route| {
+//!         // Your routes here...
+//! #       let _ = route;
+//!     })
+//! }
+//! #
+//! # fn main() { router(); }
+//! ```
+//!
+//! # Running a query
+//!
+//! At the time of writing, Diesel only supports synchronous database queries. To avoid blocking
+//! the event loop, this middleware provides a `run_with_diesel` function, which executes queries
+//! via the thread pool provided by `WorkersMiddleware`.
+//!
+//! ```rust
+//! # extern crate gotham;
+//! # extern crate gotham_middleware_workers;
+//! # extern crate gotham_middleware_diesel;
+//! # extern crate diesel;
+//! # extern crate futures;
+//! # extern crate hyper;
+//! # extern crate mime;
+//! #
+//! # use gotham::handler::*;
+//! # use gotham::http::response::*;
+//! # use gotham::router::Router;
+//! # use gotham::router::builder::*;
+//! # use gotham::pipeline::single::*;
+//! # use gotham::pipeline::*;
+//! # use gotham::state::*;
+//! # use gotham::test::*;
+//! # use gotham_middleware_workers::*;
+//! # use gotham_middleware_diesel::*;
+//! # use futures::*;
+//! # use hyper::StatusCode;
+//! # use diesel::{RunQueryDsl, SqliteConnection};
+//! #
+//! # fn router() -> Router {
+//! #   let (chain, pipelines) = single_pipeline(
+//! #       new_pipeline()
+//! #           .add(WorkersMiddleware::new(20))
+//! #           .add(DieselMiddleware::<SqliteConnection>::new(":memory:"))
+//! #           .build()
+//! #   );
+//! #
+//! #   build_router(chain, pipelines, |route| {
+//! #       route.get("/").to(handler);
+//! #   })
+//! # }
+//! #
+//! fn handler(state: State) -> Box<HandlerFuture> {
+//!     // Ownership of `state` is taken by `run_with_diesel`. Since we can't
+//!     // pass it to other threads, it can't be captured by the closure. The
+//!     // `state` value will be yielded by the future upon success or error.
+//!     let f = run_with_diesel(state, |conn: &SqliteConnection| {
+//!         // In this context, we are on a background thread. We can do
+//!         // synchronous operations without blocking the event loop.
+//!         //
+//!         // As an example, we perform the query:
+//!         // `SELECT 1`
+//!         //
+//!         // The result is loaded into a single `i64` which is yielded by the
+//!         // future upon successful completion.
+//!         diesel::select(diesel::dsl::sql("1"))
+//!             .load::<i64>(conn)
+//!             .map(|v| v.into_iter().next().expect("no results"))
+//!     }).then(|r| {
+//!         // Continuations to the future returned from `run_with_diesel` will
+//!         // be run on the event loop. Note that we no longer have access to
+//!         // the `conn` value, but we've now regained our `state` via `r`
+//!         // (which is a `Result<(State, i64), (State, HandlerError)>`).
+//!         //
+//!         // We unwrap `r` to get `state` and the result from the query, and
+//!         // then render it into the response body.
+//!         let (state, n) = r.unwrap_or_else(|_| panic!("query failed"));
+//!         let body = format!("result: {}", n);
+//!
+//!         // Use Gotham's `create_response` helper to populate the response.
+//!         let response = create_response(
+//!             &state,
+//!             StatusCode::Ok,
+//!             Some((body.into_bytes(), mime::TEXT_PLAIN)),
+//!         );
+//!
+//!         // Complete the future with an immediately available value.
+//!         Ok((state, response))
+//!     });
+//!
+//!     Box::new(f)
+//! }
+//! #
+//! # fn main() {
+//! #   let test_server = TestServer::new(router()).unwrap();
+//! #   let response = test_server
+//! #       .client()
+//! #       .get("https://example.com/")
+//! #       .perform()
+//! #       .unwrap();
+//! #   assert_eq!(response.status(), StatusCode::Ok);
+//! #   let body = response.read_utf8_body().unwrap();
+//! #   assert_eq!(&body, "result: 1");
+//! # }
+//! ```
 
 #![warn(missing_docs, deprecated)]
 #![doc(test(no_crate_inject, attr(deny(warnings))))]
