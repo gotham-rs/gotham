8d0f2458ad4c0b16a62ddc7c470dca93742e8e5d
Ivan Mironov <mironov.ivan@gmail.com>
2018-05-10 02:29:36 +0300

    Implement run_until() similar to hyper's run_until().
    This adds the ability to gracefully shut down the event loop using
    special signaling future.

--- gotham/src/os/mod.rs
+++ gotham/src/os/mod.rs
@@ -7,3 +7,201 @@ pub use self::unix as current;
 pub mod windows;
 #[cfg(windows)]
 pub use self::windows as current;
+
+use std::cell::RefCell;
+use std::fmt::Debug;
+use std::io;
+use std::rc::{Rc, Weak};
+use std::thread::JoinHandle;
+use std::time::Duration;
+
+use futures::{task, Async, Future, Poll};
+use futures::sync::oneshot;
+use hyper::server::Service;
+use tokio_core::reactor::{Core, Timeout};
+
+pub(crate) struct ConnectionCounter {
+    connections: usize,
+    blocker: Option<task::Task>,
+}
+
+impl ConnectionCounter {
+    fn new() -> Self {
+        Self {
+            connections: 0,
+            blocker: None,
+        }
+    }
+}
+
+pub(crate) struct CountedConnection<S> {
+    counter: Weak<RefCell<ConnectionCounter>>,
+    service: S,
+}
+
+impl<S> CountedConnection<S> {
+    fn new(counter: &Rc<RefCell<ConnectionCounter>>, service: S) -> Self {
+        {
+            let mut counter = counter.borrow_mut();
+            counter.connections += 1;
+            trace!(
+                "Connection created ({} connections total)",
+                counter.connections
+            );
+        }
+        Self {
+            counter: Rc::downgrade(counter),
+            service,
+        }
+    }
+}
+
+impl<S> Drop for CountedConnection<S> {
+    fn drop(&mut self) {
+        let counter = match self.counter.upgrade() {
+            Some(counter) => counter,
+            None => return,
+        };
+        let mut counter = counter.borrow_mut();
+        counter.connections -= 1;
+        trace!(
+            "Connection destroyed ({} connections left)",
+            counter.connections
+        );
+        if counter.connections == 0 {
+            if let Some(task) = counter.blocker.take() {
+                task.notify();
+            }
+        }
+    }
+}
+
+impl<S> Service for CountedConnection<S>
+where
+    S: Service,
+{
+    type Request = S::Request;
+    type Response = S::Response;
+    type Error = S::Error;
+    type Future = S::Future;
+
+    fn call(&self, req: Self::Request) -> Self::Future {
+        self.service.call(req)
+    }
+}
+
+#[derive(Clone)]
+pub(crate) struct WaitUntilZeroConnections(Rc<RefCell<ConnectionCounter>>);
+
+impl WaitUntilZeroConnections {
+    pub(crate) fn new() -> Self {
+        WaitUntilZeroConnections(Rc::new(RefCell::new(ConnectionCounter::new())))
+    }
+
+    pub(crate) fn count_connection<S>(&self, service: S) -> CountedConnection<S> {
+        CountedConnection::new(&self.0, service)
+    }
+
+    pub(crate) fn num_connections(&self) -> usize {
+        self.0.borrow().connections
+    }
+}
+
+impl Future for WaitUntilZeroConnections {
+    type Item = ();
+    type Error = io::Error;
+
+    fn poll(&mut self) -> Poll<(), io::Error> {
+        let mut counter = self.0.borrow_mut();
+        if counter.connections == 0 {
+            Ok(().into())
+        } else {
+            counter.blocker = Some(task::current());
+            Ok(Async::NotReady)
+        }
+    }
+}
+
+pub(crate) fn propagate_shutdown_signal<'a, F>(
+    shutdown_signal: F,
+    threads_shutdown_tx: Vec<oneshot::Sender<()>>,
+) -> Box<'a + Future<Item = (), Error = io::Error>>
+where
+    F: 'a + Future<Item = (), Error = ()>,
+{
+    // Accept either value (both Ok or Err) as a valid signal.
+    Box::new(shutdown_signal.then(|_| {
+        threads_shutdown_tx.into_iter().for_each(|shutdown_tx| {
+            shutdown_tx.send(()).unwrap_or_else(|_| {
+                warn!("Receiver of shutdown signal for child thread was dropped")
+            });
+        });
+        Ok(())
+    }))
+}
+
+pub(crate) fn run_until_shutdown<E, F, SE, SF>(
+    core: &mut Core,
+    srv: F,
+    shutdown_signal: SF,
+    expect_msg: &str,
+) where
+    E: Debug,
+    F: Future<Item = (), Error = E>,
+    SE: Debug,
+    SF: Future<Item = (), Error = SE>,
+{
+    let shutdown_signal =
+        shutdown_signal.map_err(|_| panic!("Sender of shutdown signal was dropped"));
+    match core.run(shutdown_signal.select(srv)) {
+        Ok(((), _)) => Ok(()),
+        Err((error, _)) => Err(error),
+    }.expect(expect_msg);
+}
+
+pub(crate) fn maybe_wait_for_remaining_connections(
+    mut core: Core,
+    wait_until_zero_connections: &WaitUntilZeroConnections,
+    shutdown_timeout: Duration,
+) {
+    if shutdown_timeout == Duration::default() {
+        debug!(
+            target: "gotham::start",
+            "Shutting down immediately, without waiting for remaining connections \
+            (connections left: {})",
+            wait_until_zero_connections.num_connections(),
+        );
+    } else {
+        let handle = core.handle();
+
+        // Wait for remaining active connections.
+        debug!(
+            target: "gotham::start",
+            "Shutting down and waiting for remaining active connections \
+            (connections left: {}, timeout: {:?})",
+            wait_until_zero_connections.num_connections(),
+            shutdown_timeout,
+        );
+
+        let timeout = Timeout::new(shutdown_timeout, &handle).expect("unable to set the timeout");
+        match core.run(wait_until_zero_connections.clone().select(timeout)) {
+            Ok(_) => Ok(()),
+            Err((error, _)) => Err(error),
+        }.expect("unable to wait for remaining active connections");
+
+        debug!(
+            target: "gotham::start",
+            "Done waiting for remaining active connections (connections left: {})",
+            wait_until_zero_connections.num_connections(),
+        );
+    }
+}
+
+pub(crate) fn join_threads<T>(threads_handles: Vec<JoinHandle<T>>) {
+    threads_handles.into_iter().for_each(|thread| {
+        thread
+            .join()
+            .map(|_| ())
+            .unwrap_or_else(|error| warn!("Unable to join child thread: {:?}", error))
+    });
+}
--- gotham/src/os/windows.rs
+++ gotham/src/os/windows.rs
@@ -2,15 +2,20 @@ use std::io;
 use std::net::{SocketAddr, TcpListener, ToSocketAddrs};
 use std::thread;
 use std::sync::{Arc, Mutex};
+use std::time::Duration;
+use std::fmt::Debug;
 
 use hyper::server::Http;
 use tokio_core;
 use tokio_core::net::TcpStream;
 use tokio_core::reactor::{Core, Handle};
 use futures::{future, task, Async, Future, Poll, Stream};
+use futures::sync::oneshot;
 
 use handler::NewHandler;
 use service::GothamService;
+use os::{join_threads, maybe_wait_for_remaining_connections, propagate_shutdown_signal,
+         run_until_shutdown, WaitUntilZeroConnections};
 
 use crossbeam::sync::SegQueue;
 
@@ -40,15 +45,26 @@ impl Stream for SocketQueue {
     }
 }
 
-/// Starts a Gotham application, with the given number of threads.
+/// Starts a Gotham application, with the given number of threads and ability to gracefully shut down.
+///
+/// This function blocks current thread until `shutdown_signal` resolved or panic occur.
+///
+/// When `shutdown_timeout` is not equal to `Duration::default()`, function waits for remaining open connections to
+/// finish for specified time.
 ///
 /// ## Windows
 ///
 /// An additional thread is used on Windows to accept connections.
-pub fn start_with_num_threads<NH, A>(addr: A, threads: usize, new_handler: NH)
-where
+pub fn run_with_num_threads_until<NH, A, F>(
+    addr: A,
+    threads: usize,
+    new_handler: NH,
+    shutdown_signal: F,
+    shutdown_timeout: Duration,
+) where
     NH: NewHandler + 'static,
     A: ToSocketAddrs,
+    F: Future<Item = (), Error = ()>,
 {
     let (listener, addr) = ::tcp_listener(addr);
 
@@ -57,12 +73,19 @@ where
 
     let queue = SocketQueue::new();
 
+    let mut threads_shutdown_tx = Vec::new();
+    let mut threads_handles = Vec::new();
+
     {
         let queue = queue.clone();
-        thread::Builder::new()
-            .name("gotham-listener".into())
-            .spawn(move || start_listen_core(listener, addr, queue))
-            .expect("unable to spawn thread");
+        let (shutdown_tx, shutdown_rx) = oneshot::channel();
+        threads_shutdown_tx.push(shutdown_tx);
+        threads_handles.push(
+            thread::Builder::new()
+                .name("gotham-listener".into())
+                .spawn(move || start_listen_core(listener, addr, queue, shutdown_rx))
+                .expect("unable to spawn thread"),
+        );
     }
 
     info!(
@@ -76,20 +99,68 @@ where
         let protocol = protocol.clone();
         let queue = queue.clone();
         let new_handler = new_handler.clone();
-        thread::Builder::new()
-            .name(format!("gotham-{}", thread_n))
-            .spawn(move || start_serve_core(queue, &protocol, new_handler))
-            .expect("unable to spawn thread");
+        let (shutdown_tx, shutdown_rx) = oneshot::channel();
+        threads_shutdown_tx.push(shutdown_tx);
+        threads_handles.push(
+            thread::Builder::new()
+                .name(format!("gotham-{}", thread_n))
+                .spawn(move || {
+                    start_serve_core(queue, &protocol, new_handler, shutdown_rx, shutdown_timeout)
+                })
+                .expect("unable to spawn thread"),
+        );
     }
 
-    start_serve_core(queue, &protocol, new_handler);
+    let shutdown_signal = propagate_shutdown_signal(shutdown_signal, threads_shutdown_tx);
+    start_serve_core(
+        queue,
+        &protocol,
+        new_handler,
+        shutdown_signal,
+        shutdown_timeout,
+    );
+    join_threads(threads_handles);
 }
 
-fn start_listen_core(listener: TcpListener, addr: SocketAddr, queue: SocketQueue) {
+/// Starts a Gotham application, with the given number of threads.
+///
+/// This function never return but may panic because of errors.
+///
+/// ## Windows
+///
+/// An additional thread is used on Windows to accept connections.
+pub fn start_with_num_threads<NH, A>(addr: A, threads: usize, new_handler: NH)
+where
+    NH: NewHandler + 'static,
+    A: ToSocketAddrs,
+{
+    run_with_num_threads_until(
+        addr,
+        threads,
+        new_handler,
+        future::empty(),
+        Duration::default(),
+    )
+}
+
+fn start_listen_core<E, F>(
+    listener: TcpListener,
+    addr: SocketAddr,
+    queue: SocketQueue,
+    shutdown_signal: F,
+) where
+    E: Debug,
+    F: Future<Item = (), Error = E>,
+{
     let mut core = Core::new().expect("unable to spawn tokio reactor");
     let handle = core.handle();
-    core.run(listen(listener, addr, queue, &handle))
-        .expect("unable to run reactor over listener");
+    let srv = listen(listener, addr, queue, &handle);
+    run_until_shutdown(
+        &mut core,
+        srv,
+        shutdown_signal,
+        "unable to run reactor over listener",
+    );
 }
 
 fn listen(
@@ -116,14 +187,36 @@ fn listen(
     }))
 }
 
-fn start_serve_core<NH>(queue: SocketQueue, protocol: &Http, new_handler: Arc<NH>)
-where
+fn start_serve_core<NH, E, F>(
+    queue: SocketQueue,
+    protocol: &Http,
+    new_handler: Arc<NH>,
+    shutdown_signal: F,
+    shutdown_timeout: Duration,
+) where
     NH: NewHandler + 'static,
+    E: Debug,
+    F: Future<Item = (), Error = E>,
 {
     let mut core = Core::new().expect("unable to spawn tokio reactor");
     let handle = core.handle();
-    core.run(serve(queue, protocol, new_handler, &handle))
-        .expect("unable to run reactor for work stealing");
+
+    let wait_until_zero_connections = WaitUntilZeroConnections::new();
+
+    let srv = serve(
+        queue,
+        protocol,
+        new_handler,
+        &handle,
+        &wait_until_zero_connections,
+    );
+    run_until_shutdown(
+        &mut core,
+        srv,
+        shutdown_signal,
+        "unable to run reactor for work stealing",
+    );
+    maybe_wait_for_remaining_connections(core, &wait_until_zero_connections, shutdown_timeout);
 }
 
 fn serve<'a, NH>(
@@ -131,6 +224,7 @@ fn serve<'a, NH>(
     protocol: &'a Http,
     new_handler: Arc<NH>,
     handle: &'a Handle,
+    wait_until_zero_connections: &'a WaitUntilZeroConnections,
 ) -> Box<Future<Item = (), Error = ()> + 'a>
 where
     NH: NewHandler + 'static,
@@ -148,7 +242,12 @@ where
         }).and_then(move |_| {
             queue.for_each(move |(socket, addr)| {
                 let service = gotham_service.connect(addr);
-                let f = protocol.serve_connection(socket, service).then(|_| Ok(()));
+                let f = protocol
+                    .serve_connection(
+                        socket,
+                        wait_until_zero_connections.count_connection(service),
+                    )
+                    .then(|_| Ok(()));
 
                 handle.spawn(f);
                 Ok(())
