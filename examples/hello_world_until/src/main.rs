bd20f98b2fe540a47bd6e14e68c750340a2d4a9a
Ivan Mironov <mironov.ivan@gmail.com>
2018-07-08 02:14:45 +0300

    Implement example of graceful shutdown.

--- examples/hello_world_until/src/main.rs
+++ examples/hello_world_until/src/main.rs
@@ -1,13 +1,19 @@
 //! A Hello World example application for working with Gotham.
+//! Supports graceful shutdown on Ctrl+C.
 
 extern crate gotham;
 extern crate hyper;
 extern crate mime;
+extern crate tokio;
+extern crate tokio_signal;
 
 use hyper::{Response, StatusCode};
 
 use gotham::helpers::http::response::create_response;
 use gotham::state::State;
+
+use tokio::prelude::Future;
+use tokio::prelude::Stream;
 
 /// Create a `Handler` which is invoked when responding to a `Request`.
 ///
@@ -26,9 +32,31 @@ pub fn say_hello(state: State) -> (State, Response) {
 
 /// Start a server and call the `Handler` we've defined above for each `Request` we receive.
 pub fn main() {
+    // Future to wait for Ctrl+C.
+    let signal = tokio_signal::ctrl_c()
+        .flatten_stream()
+        .map_err(|error| panic!("Error listening for signal: {}", error))
+        .take(1)
+        .for_each(|()| {
+            println!("Ctrl+C pressed");
+            Ok(())
+        });
+
     let addr = "127.0.0.1:7878";
     println!("Listening for requests at http://{}", addr);
-    gotham::start(addr, || Ok(say_hello))
+    println!("Press Ctrl+C to exit");
+
+    let server = gotham::create_server(addr, || Ok(say_hello));
+    // Wait either for server to finish (never happens) or for signal.
+    // We are not interested on the other future, so we drop it using map() and map_err().
+    let with_signal = server
+        .select(signal)
+        .map(|(ok, _)| ok)
+        .map_err(|(error, _)| error);
+
+    tokio::run(with_signal);
+
+    println!("Shutting down gracefully");
 }
 
 #[cfg(test)]
