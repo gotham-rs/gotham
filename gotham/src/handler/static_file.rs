525ad0135fad2edc848c578c6c8ff77b055e3c8e
colinbankier <colinbankier@gmail.com>
2018-08-24 00:08:18 +1000

    Handle etag.

--- gotham/src/handler/static_file.rs
+++ gotham/src/handler/static_file.rs
@@ -4,6 +4,9 @@ use futures::{stream, Future, Stream};
 use handler::{Handler, HandlerFuture, IntoHandlerError, NewHandler};
 use helpers::http::response::create_response;
 use http;
+use hyper::header::{
+    HeaderMap, HeaderValue, ACCEPT_ENCODING, ETAG, IF_MODIFIED_SINCE, IF_NONE_MATCH, LAST_MODIFIED,
+};
 use hyper::{Body, Chunk, Response, StatusCode};
 use mime::{self, Mime};
 use mime_guess::guess_mime_type_opt;
@@ -15,6 +18,7 @@ use std::fs::Metadata;
 use std::io;
 use std::iter::FromIterator;
 use std::path::{Component, Path, PathBuf};
+use std::time::UNIX_EPOCH;
 use tokio::fs::File;
 use tokio::io::AsyncRead;
 
@@ -90,10 +94,11 @@ impl Handler for FileHandler {
 
 fn create_file_response(path: PathBuf, state: State) -> Box<HandlerFuture> {
     let mime_type = mime_for_path(&path);
+    let headers = HeaderMap::borrow_from(&state).clone();
 
     let response_future = File::open(path).and_then(|file| file.metadata()).and_then(
         move |(file, meta)| {
-            if not_modified() {
+            if not_modified(&meta, &headers) {
                 Ok(http::Response::builder()
                     .status(StatusCode::NOT_MODIFIED)
                     .body("".into())
@@ -131,10 +136,6 @@ fn error_status(e: &io::Error) -> StatusCode {
     }
 }
 
-fn not_modified() -> bool {
-    false
-}
-
 fn mime_for_path(path: &Path) -> Mime {
     guess_mime_type_opt(path).unwrap_or_else(|| mime::APPLICATION_OCTET_STREAM)
 }
@@ -152,6 +153,38 @@ fn normalize_path(path: &Path) -> PathBuf {
             }
             _ => result,
         })
+}
+
+fn not_modified(metadata: &Metadata, headers: &HeaderMap) -> bool {
+    // If-None-Match header takes precedence over If-Modified-Since
+    if let Some(etag) = entity_tag(&metadata) {
+        let if_none_match = headers.get_all(IF_NONE_MATCH);
+        if if_none_match.iter().any(|v| v == &etag) {
+            return true;
+        }
+    };
+    // } else if let Some(IfModifiedSince(if_modified_time)) = if_modified_since {
+    //     metadata
+    //         .modified()
+    //         .map(|modified| HttpDate::from(modified) <= if_modified_time)
+    //         .unwrap_or(false)
+    // } else {
+    //     false
+    // }
+    false
+}
+
+fn entity_tag(metadata: &Metadata) -> Option<String> {
+    metadata.modified().ok().and_then(|modified| {
+        modified.duration_since(UNIX_EPOCH).ok().map(|duration| {
+            format!(
+                "W/\"{0:x}-{1:x}.{2:x}\"",
+                metadata.len(),
+                duration.as_secs(),
+                duration.subsec_nanos()
+            )
+        })
+    })
 }
 
 /// Responsible for extracting the file path matched by the glob segment from the URL.
@@ -319,6 +352,32 @@ mod tests {
 
         let body = response.read_body().unwrap();
         assert_eq!(&body[..], b"<html>I am a doc.</html>");
+    }
+
+    #[test]
+    fn static_not_modified_etag() {
+        use std::fs::File;
+
+        let path = "resources/test/static_files/doc.html";
+        let test_server =
+            TestServer::new(build_simple_router(|route| route.get("/").to_file(path))).unwrap();
+
+        let etag = File::open(path)
+            .and_then(|file| file.metadata())
+            .map(|meta| super::entity_tag(&meta).expect("entity tag"))
+            .unwrap();
+
+        let response = test_server
+            .client()
+            .get("http://localhost/")
+            .with_header(
+                "If-None-Match",
+                HeaderValue::from_bytes(etag.as_bytes()).unwrap(),
+            )
+            .perform()
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::NOT_MODIFIED);
     }
 
     fn test_server() -> TestServer {
