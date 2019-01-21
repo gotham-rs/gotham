94d27288427e504db00e9dde60957658a2c2a90b
Nicholas <nyoung@uptime.ventures>
2019-01-21 11:39:17 -0700

    Constants no longer require `static` lifetimes

--- gotham/src/helpers/http/header/mod.rs
+++ gotham/src/helpers/http/header/mod.rs
@@ -2,7 +2,7 @@
 //! provided by the Hyper library.
 
 /// Marks the identifier of a request to a Gotham server.
-pub const X_REQUEST_ID: &'static str = "x-request-id";
+pub const X_REQUEST_ID: &str = "x-request-id";
 
 /// Marks the execution time of a Gotham request.
-pub const X_RUNTIME_DURATION: &'static str = "x-runtime-duration";
+pub const X_RUNTIME_DURATION: &str = "x-runtime-duration";
