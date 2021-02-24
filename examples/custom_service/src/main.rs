92113224ae14901fe82f6086717c9f3d6ca8fac5
Thomas Orozco <torozco@fb.com>
2021-02-24 12:01:02 +0000

    state: move instantiation into State::from_request
    This refactors the service implementation a little bit to instantiate
    the State via a dedicated method. The underlying goal is to allow users
    to use Gotham without having to use its handler.
    
    The motivation for that is to allow:
    
    - Not requiring handlers to be unwind safe.
    - Injecting extra things into the state that came from the socket
      (besides the client address), such as client TLS certificates.

---
+++ examples/custom_service/src/main.rs
@@ -0,0 +1,78 @@
+//! An example usage of Gotham from another service.
+
+use anyhow::{Context as _, Error};
+use futures::future::{BoxFuture, FutureExt};
+use gotham::{
+    router::{builder::*, Router},
+    service::call_handler,
+    state::State,
+};
+use http::{Request, Response};
+use hyper::{server::conn::Http, service::Service, Body};
+use std::net::SocketAddr;
+use std::panic::AssertUnwindSafe;
+use std::task;
+use tokio::net::TcpListener;
+
+#[derive(Clone)]
+struct MyService {
+    router: Router,
+    addr: SocketAddr,
+}
+
+impl Service<Request<Body>> for MyService {
+    type Response = Response<Body>;
+    type Error = Error;
+    type Future = BoxFuture<'static, Result<Self::Response, Self::Error>>;
+
+    fn poll_ready(&mut self, _cx: &mut task::Context<'_>) -> task::Poll<Result<(), Self::Error>> {
+        task::Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, req: Request<Body>) -> Self::Future {
+        // NOTE: You don't *have* to use call_handler for this (you could use `router.handle`), but
+        // call_handler will catch panics and return en error response.
+        let state = State::from_request(req, self.addr);
+        call_handler(self.router.clone(), AssertUnwindSafe(state)).boxed()
+    }
+}
+
+pub fn say_hello(state: State) -> (State, &'static str) {
+    (state, "hello world")
+}
+
+#[tokio::main]
+pub async fn main() -> Result<(), Error> {
+    let router = build_simple_router(|route| {
+        // For the path "/" invoke the handler "say_hello"
+        route.get("/").to(say_hello);
+    });
+
+    let addr = "127.0.0.1:7878";
+    let listener = TcpListener::bind(&addr).await?;
+
+    println!("Listening for requests at http://{}", addr);
+
+    loop {
+        let (socket, addr) = listener
+            .accept()
+            .await
+            .context("Error accepting connection")?;
+
+        let service = MyService {
+            router: router.clone(),
+            addr,
+        };
+
+        let task = async move {
+            Http::new()
+                .serve_connection(socket, service)
+                .await
+                .context("Error serving connection")?;
+
+            Result::<_, Error>::Ok(())
+        };
+
+        tokio::spawn(task);
+    }
+}
