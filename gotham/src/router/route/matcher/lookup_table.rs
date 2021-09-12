74f942273828f4f65403448635ada13b3b41a468
Dominic <git@msrd0.de>
2021-09-12 14:37:38 +0200

    simplify the code even more

--- gotham/src/router/route/matcher/lookup_table.rs
+++ gotham/src/router/route/matcher/lookup_table.rs
@@ -1,5 +1,4 @@
 use mime::Mime;
-use std::array;
 use std::collections::HashMap;
 
 /// This type is used to quickly lookup a non-hashed container of mime types by their essence string.
@@ -21,20 +20,12 @@ where
 impl LookupTableFromTypes for LookupTable {
     fn from_types<'a, I: Iterator<Item = &'a Mime>>(types: I, include_stars: bool) -> Self {
         let mut tbl = Self::new();
-        if include_stars {
-            for (key, value) in types.enumerate().flat_map(|(i, mime)| {
-                array::IntoIter::new([
-                    ("*/*".to_owned(), i),
-                    (format!("{}/*", mime.type_()), i),
-                    (mime.essence_str().to_owned(), i),
-                ])
-            }) {
-                insert(&mut tbl, key, value);
+        for (i, mime) in types.enumerate() {
+            if include_stars {
+                insert(&mut tbl, "*/*", i);
+                insert(&mut tbl, format!("{}/*", mime.type_()), i);
             }
-        } else {
-            for (i, mime) in types.enumerate() {
-                insert(&mut tbl, mime.essence_str(), i);
-            }
+            insert(&mut tbl, mime.essence_str(), i);
         }
         tbl
     }
